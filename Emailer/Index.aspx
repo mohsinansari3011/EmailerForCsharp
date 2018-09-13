ec3eb3efe105ea9bc2c278c03c3cdfb5435c324f
Mohsin <mohsinansari3011@gmail.com>
2018-09-13 21:46:53 +0500

    Email Sender with C#
    how to send email in c#, asp.net
    
    #CSharp #asp.net

---
+++ Emailer/Index.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs"  ValidateRequest="false" Inherits="Emailer.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Email Checker </title>
+    <link rel="shortcut icon" href="favicon.ico" />
+
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" rel="stylesheet" />
+
+</head>
+<body  style="background-image:url('Emailer.jpg')">
+
+    <div class="container">
+
+        <div class="row" style="margin-top:15px;"> 
+
+            <form id="form1" runat="server">
+
+                 <div class="form-group">
+                <asp:Button Text="Send Email" CssClass="btn btn-primary" id="btnsubmit" OnClick="btnsubmit_Click" runat="server" />
+                     </div>
+                 <div class="form-group">
+                    <asp:TextBox CssClass="form-control" runat="server" ID="txtbcc" aria-describedby="emailHelp" placeholder="BCC"/>
+                   
+                </div>
+
+                <div class="form-group">
+                    <asp:Label ForeColor="Red" Text="Please Paste html content to send it as an email!!" ID="lblMessage" runat="server" />
+                    <asp:TextBox CssClass="form-control" runat="server" Columns="100" Rows="15" ID="txtemail" TextMode="MultiLine"  aria-describedby="emailHelp" placeholder="Email Content"/>
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+       
+        
+
+        
+
+    </form>
+
+        </div>
+
+    </div>
+
+    
+</body>
+</html>
---
+++ Emailer/Index.aspx.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Sockets;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Emailer
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        public class EmailCredentials
+        {
+            public string From { get; set; }
+            public string To { get; set; }
+            public string BCC { get; set; }
+            public string Mail { get; set; }
+            public string Subject { get; set; }
+            public string SMTPMail { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public int _Port { get; set; }
+            public bool _SSL { get; set; }
+
+        }
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+
+       
+       
+    
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtemail.Text.Length > 0)
+                {
+                    string myIP = string.Empty;
+                    try
+                    {
+                        string hostName = Dns.GetHostName();
+                        myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();  
+                    }
+                    catch (Exception)
+                    {
+                    }
+                   
+
+
+                    EmailCredentials objEmail = new EmailCredentials();
+
+                    System.Net.Mime.ContentType mimeType = new System.Net.Mime.ContentType("text/html");
+
+                    objEmail.From = "From Email Address";
+                    objEmail.UserName = "username";
+                    objEmail.Password = "password";
+                    objEmail.To = "mohsinansari3011@gmail.com";
+
+                    if (txtbcc.Text.Trim().Length>0)
+                    {
+                        objEmail.BCC = txtbcc.Text.Trim();
+                    }
+                    
+                    
+                    
+                    
+                    objEmail.SMTPMail = "smtp.gmail.com";
+                    objEmail._Port = 587;
+                    objEmail._SSL = true;
+                    objEmail.Subject = "This Email is generated from Email tester!! by " + myIP;
+                    objEmail.Mail = txtemail.Text.Trim();
+
+
+                    if (SendMail(objEmail))
+                    {
+                        lblMessage.Text = "Email Sent Successfully!!!";
+                    }
+                    else
+                    {
+                        lblMessage.Text = "Email Sending Error!!";
+                    }
+                }
+                else
+                {
+                    lblMessage.Text = "Please Paste html content to send it as an email!!";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                
+                 lblMessage.Text = ex.Message;
+            }
+        }
+
+
+        public bool SendMail(EmailCredentials objEmail)
+        {
+            bool Issent = false;
+
+            try
+            {
+                MailMessage MailMsg = new MailMessage();
+                MailMsg.Subject = objEmail.Subject;
+                MailMsg.From = new MailAddress(objEmail.From);
+
+                if (objEmail.To.Contains(","))
+                {
+                    foreach (var EmailTo in objEmail.To.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        MailMsg.To.Add(new MailAddress(EmailTo));
+                    }
+                }
+                else
+                {
+                    MailMsg.To.Add(new MailAddress(objEmail.To));
+                }
+
+
+                if (objEmail.BCC != null)
+                {
+                    if (objEmail.BCC.Length > 0)
+                    {
+                        if (objEmail.BCC.Contains(","))
+                        {
+                            foreach (var EmailBCC in objEmail.BCC.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries))
+                            {
+                                MailMsg.Bcc.Add(new MailAddress(EmailBCC));
+                            }
+                        }
+                        else
+                        {
+                            MailMsg.Bcc.Add(new MailAddress(objEmail.BCC));
+                        }
+                    }
+                    
+                }
+                
+
+
+                //MailMsg.To.Add(new MailAddress(To));
+                MailMsg.Priority = MailPriority.High;
+                MailMsg.Body = objEmail.Mail;
+                MailMsg.IsBodyHtml = true;
+                SmtpClient SmtpMail = new SmtpClient();
+                SmtpMail.Host = objEmail.SMTPMail;
+                SmtpMail.UseDefaultCredentials = false;
+                NetworkCredential basicInfo = new NetworkCredential(objEmail.UserName, objEmail.Password);
+                SmtpMail.Credentials = basicInfo;
+                SmtpMail.Port = objEmail._Port;
+                SmtpMail.EnableSsl = objEmail._SSL;
+                SmtpMail.Send(MailMsg);
+
+                Issent = true;
+            }
+            catch (Exception)
+            {
+                Issent = false;
+            }
+
+            return Issent;
+        }
+    }
+}
